950b138c8ab41bc49f351325ae0281ca6ace29e3
quoc <bui.vquoc@gmail.com>
2020-12-30 22:08:52 +0700

    Stateless functional components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import './App.css';
 
+// Stateless Functional Component
+const Box = (props) => {
+  return ( 
+    <div className="box">
+      <h1>{props.text}</h1>
+    </div>
 
+  );
+}
+const App = () => {
+  return (
+    <Box text="This is an box"/>
+  );
+}
 
 export default App;
